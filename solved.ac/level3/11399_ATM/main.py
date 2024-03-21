3c1c2556bec2191624fe273e0e556b1dec548b9d
Kimjae-1 <kimjae1@g.skku.edu>
2024-03-21 22:29:28 +0900

    atm

--- solved.ac/level3/11399_ATM/main.py
+++ solved.ac/level3/11399_ATM/main.py
@@ -1,3 +1,14 @@
 import sys
-sys.stdin = open('./solved.ac/11399_ATM/input.txt', 'r')
+sys.stdin = open('./solved.ac/level3/11399_ATM/input.txt', 'r')
 
+N = int(input())
+lst = sorted(list(map(int,input().split())),reverse = True)
+
+time = []
+for i in range(N):
+    time.append((i+1) * lst[i])
+    
+print(sum(time))
+    
+    
+    
